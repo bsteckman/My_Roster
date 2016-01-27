039082ed8c5ecd1eb7176dfbb556c5a445f9d4e2
Brett Steckman <eveningside@gmail.com>
2016-01-27 02:17:18 -0700

    constructor actually puts player into roster

--- script/app.js
+++ script/app.js
@@ -1,5 +1,28 @@
-function makePlayerCard(name,position,num){
+var roster = {
+    players: []
+}
+
+
+var playerListElem = document.getElementById('player-list');
+
+
+function Player(name, pos, num, url) {
+    
     this.name = name;
-    this.position = position;
-    this.num
+    this.pos = pos;
+    this.num = num;
+    this.url = url;
+    roster.players.push(this);
+  } 
+
+for(var i = 0; i < roster.players.length; i++) {
+    var currentPlayer = roster.players[i];
+    drawPlayerOnScreen(currentPlayer);
+}
+
+function drawPlayerOnScreen(currentPlayer) {
+    var playerElem = document.createElement('div');
+    playerElem.className = 'player-card';
+    playerElem.innerHTML = '<button type="button" class="btn btn-danger"> Remove Player </button>' + '<p>' + currentPlayer.name + '</p> <p>' + currentPlayer.pos + '</p> <img src="' + currentPlayer.url + '" alt="assets\images\blankplayer.png"><div class="btn-group"><button>Add to Cart</button></div>'
+    playerListElem.appendChild(playerElem);
 }
---
+++ script/jquery.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+// var buttons = jQuery('button');
+
+// function grabItem (e){
+//     var btn = $(e.target);
+//     var productId = btn.closest('.product').attr('id');
+//     for(var i = 0; i < store.products.length; i++){
+//        var currentProduct = store.products[i];
+//        if(currentProduct.id == productId && currentProduct.inStock()){
+//            cart.addProduct(currentProduct);
+//            currentProduct.quantity--;
+//            console.log(cart.products);
+//        } 
+//     }
+    
+// }
+
+// buttons.click(grabItem);
