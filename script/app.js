bbe3c0b37376ce0669651d0f51bcb7b722bbc882
Brett Steckman <eveningside@gmail.com>
2016-01-28 16:02:30 -0700

    IT LIVES!!!!!

--- script/app.js
+++ script/app.js
@@ -1,32 +1,27 @@
-var roster = {
-       players: []
-}
+var roster = []
 
+var playerListElem = $('#roster');
+var playerId = 0;
 
-var playerListElem = document.getElementById('roster');
-
-
-function Player(name, pos, num) {
-    
+function Player(id, name, pos, num) {
+    this.id = playerId++;
     this.name = document.getElementById('inputname').value;
     this.pos = document.getElementById('inputposition').value;
     this.num = document.getElementById('inputnum').value;
-    roster.players.push(this);
+    roster.push(this);
     drawPlayerOnScreen(this);
   }
 
-// for(var i = 0; i < roster.players.length; i++) {
-//     var currentPlayer = roster.players[i];
-//     drawPlayerOnScreen(currentPlayer);
-// }
-
-
-
 function drawPlayerOnScreen(currentPlayer) {
-    var playerElem = document.createElement('div');
-    playerElem.className = 'player-card';
-    playerElem.innerHTML = '<div><button type="button" class="btn-block btn-danger" id="remove"> Remove Player </button></div>' +  '<img class="url" src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/">' + '<p class="playerinfo">' + "Name: " + currentPlayer.name + '</p> <p class="playerinfo">' + "Position: " + currentPlayer.pos + '</p> <p class="playerinfo">' + "# " + currentPlayer.num + '</p>' + "</div>"
-    playerListElem.appendChild(playerElem);
+    var playerElem = $('<div class="player-card">');
+    var removeBtn = $('<button type="button" class="btn-block btn-danger"> Remove Player </button>')
+    removeBtn.click(function(){
+        roster.splice(roster.indexOf(currentPlayer), 1);
+        playerElem.remove();
+    })
+    playerElem.append(removeBtn)
+    playerElem.append('<div>' +  '<img class="url" src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/">' + '<p class="playerinfo">' + "Name: " + currentPlayer.name + '</p> <p class="playerinfo">' + "Position: " + currentPlayer.pos + '</p> <p class="playerinfo">' + "# " + currentPlayer.num + '</p>' + '<p class="id" hidden>' + currentPlayer.id + '</p></div>')
+    playerListElem.append(playerElem);
 }
 
 
